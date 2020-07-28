bcc79eb135a26deb8aaea3ef506dba7985ccaa00
Anissa Shanks <anissa.shanks@gmail.com>
2020-07-27 20:38:57 -0500

    entered code prompting user to choose an 8 character password

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ var uppercase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '&', '*'];
+
+
 var workshopArr = [ ];
+var finalResultArr = [ ]; 
+
 // Write password to the #password input
+var userPasswordLengthChoice = prompt("Enter a password containing 8 characters.");
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
