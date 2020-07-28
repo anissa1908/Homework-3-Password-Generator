b71fddd6bce7e31a526bb9658d7a5f140fe7a28e
Anissa Shanks <anissa.shanks@gmail.com>
2020-07-27 20:27:15 -0500

    added variable for workshopArr

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var uppercase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '&', '*'];
+var workshopArr = [ ];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
