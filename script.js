89766acdfd45a20e27341c64bc140b0c4c5e7bfc
Anissa Shanks <anissa.shanks@gmail.com>
2020-07-27 22:16:45 -0500

    added confirms for all 4 variables

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ var uppercase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '&', '*'];
-
-
-var finalResultArr = [ ]; 
+var finalResultArr = newFunction(); 
 
 // Write password to the #password input
 var userPasswordLengthChoice = prompt("How long do you want your password to be?");
   while (length < 8 || length > 128) length = (prompt),"Length must be at least 8 characters and no more than 128."; {
     alert
   }
+
+var uppercase = confirm("Do you want to use uppercase?");
+var lowercase = confirm("Do you want to use lower case?");
+var numbers = confirm("Do you want to use numbers?");
+var special = confirm("Do you want to use special characters?");
+
+function newFunction() {
+  return [];
+}
 
   function uppercase(arr) {
     for(var i = 0; i < uppercaseArr.length; i++) {
@@ -47,6 +54,11 @@ var userPasswordLengthChoice = prompt("How long do you want your password to be?
           if (userChoiceNumber === true) {
             number(workshopArr);
           };
+
+  var workshopArr = [];
+          if (userChoiceNumber === false) {
+            alert = ("");
+          }
 
 function writePassword() {
   var password = generatePassword();
