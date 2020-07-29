c8f55f1eb05ccff97f17f15e63ecabe6c6e0c571
Anissa Shanks <anissa.shanks@gmail.com>
2020-07-28 22:26:43 -0500

    deleted unnecessary generate button code at bottom of js doc

--- script.js
+++ script.js
@@ -21,9 +21,6 @@ generateBtn.addEventListener("click"), function() {
 }
 
 // Prompting system to generate a password
-function generatePassword() {
-  return [];
-}
   function characters(arr) {
     for(var i = 0; i < characters.length; i++) {
           arr.push(characters[i]);
@@ -36,7 +33,7 @@ function generatePassword() {
           if (userPasswordLengthChoice === true) {
             number(workshopArr);
           };
-        
+      
   var workshopArr = [];
           if (userPasswordLengthChoice=== false) {
             alert= ("Your password is not long enough!");
@@ -50,6 +47,8 @@ function generatePassword() {
   //   // While there remain elements to shuffle...
   //    while (0 !== currentIndex) {
           
+
+
   //  // Pick a remaining element...
   //    randomIndex = Math.floor(Math.random() * currentIndex);
   //    currentIndex -= 1;
@@ -80,7 +79,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click"), function()
- {  generatePassword();
-}
