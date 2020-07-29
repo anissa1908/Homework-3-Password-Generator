54d2fee4a68895d4776bf13b7ccc2067dbe55ee0
Anissa Shanks <anissa.shanks@gmail.com>
2020-07-28 21:40:09 -0500

    reordered variables at top of doc

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Homework-3-Password-Generator
-Javascript homework
+For this project, we were tasked with using Javascript to generate a random password in the Password Generator.
--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var uppercase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '&', '*'];
-var finalResultArr = newFunction(); 
+var uppercase = confirm("Do you want to use uppercase?");
+var lowercase = confirm("Do you want to use lower case?");
+var numbers = confirm("Do you want to use numbers?");
+var special = confirm("Do you want to use special characters?");
+
+
+
+var finalResultArr = newFunction() {
+
+}; 
+
 
 
 // Write password to the #password input
@@ -13,12 +22,7 @@ var userPasswordLengthChoice = confirm("How long do you want your password to be
   while (length < 8 || length > 128) length = (prompt),"Length must be at least 8 characters and no more than 128."; {
     alert
   }
-
-var uppercase = confirm("Do you want to use uppercase?");
-var lowercase = confirm("Do you want to use lower case?");
-var numbers = confirm("Do you want to use numbers?");
-var special = confirm("Do you want to use special characters?");
-
+  
 function newFunction() {
   return [];
 }
@@ -62,7 +66,24 @@ function newFunction() {
             alert= ("Your password is not long enough!");
           }
 
-
+  // function shuffle(array) {
+  //     var currentIndex = array.length, temporaryValue, randomIndex;
+          
+  //   // While there remain elements to shuffle...
+  //    while (0 !== currentIndex) {
+          
+  //  // Pick a remaining element...
+  //    randomIndex = Math.floor(Math.random() * currentIndex);
+  //    currentIndex -= 1;
+          
+  // // And swap it with the current element.
+  //    temporaryValue = array[currentIndex];
+  //    array[currentIndex] = array[randomIndex];
+  //    array[randomIndex] = temporaryValue;
+  //   }
+          
+  //     return array;
+  //         }
 
 
 function writePassword() {
